05fcb2082971da4017fe0a70b74f51b25e00c223
Saksham Jain <saksham.khatod27@gmail.com>
2026-03-08 19:45:22 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -13,8 +13,6 @@ export default function RootLayout({ children }) {
           src="/tracker.js"
           data-tracker-id="a0b13b39-797f-4009-96bf-82f2c09e2704"
           data-domain="a.b"
-          data-allow-localhost="true"
-          data-debug="true"
           strategy="afterInteractive"
         />
       </body>
